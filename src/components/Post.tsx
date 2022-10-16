9dc0c680f27f6530a09f8185ead96570f876d7b6
zyyx-k-nanao <k-nanao@zyyx.jp>
2022-10-16 19:35:19 +0900

    コメントの内容をfirebaseに登録できた

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Post.module.css';
-// import firebase from 'firebase';
-// import { db } from '../firebase';
-// import { useSelector } from 'react-redux';
-// import { selectUser } from '../features/userSlice';
+import firebase from 'firebase';
+import { db } from '../firebase';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../features/userSlice';
 import { Avatar } from '@material-ui/core';
-// import { makeStyles } from '@material-ui/core/styles';
-// import MessageIcon from '@material-ui/icons/Message';
-// import SendIcon from '@material-ui/icons/Send';
+import { makeStyles } from '@material-ui/core/styles';
+import MessageIcon from '@material-ui/icons/Message';
+import SendIcon from '@material-ui/icons/Send';
 
 //typescriptでpropsで渡す場合は、データ型を定義する必要があるので
 interface PROPS {
@@ -19,6 +19,20 @@ interface PROPS {
   username: string;
 }
 const Post: React.FC<PROPS> = (props) => {
+  const user = useSelector(selectUser);
+  //コメントを制御
+  const [comment, setComment] = useState<string>('');
+  //
+  const newComment = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    db.collection('posts').doc(props.postId).collection('comments').add({
+      avatar: user.photoUrl,
+      text: comment,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      username: user.displayName,
+    });
+    setComment('');
+  };
   return (
     <div className={styles.post}>
       <div className={styles.post_avatar}>
@@ -38,12 +52,34 @@ const Post: React.FC<PROPS> = (props) => {
             <p>{props.text}</p>
           </div>
         </div>
+        {props.image && (
+          <div className={styles.post_tweetImage}>
+            <img src={props.image} alt='tweet' />
+          </div>
+        )}
+        <form onSubmit={newComment}>
+          <div className={styles.post_form}>
+            <input
+              className={styles.post_input}
+              type='text'
+              placeholder='Type new comment...'
+              value={comment}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setComment(e.target.value)
+              }
+            />
+            <button
+              disabled={!comment}
+              className={
+                comment ? styles.post_button : styles.post_buttonDisable
+              }
+              type='submit'
+            >
+              <SendIcon className={styles.post_sendIcon} />
+            </button>
+          </div>
+        </form>
       </div>
-      {props.image && (
-        <div className={styles.post_tweetImage}>
-          <img src={props.image} alt='tweet' />
-        </div>
-      )}
     </div>
   );
 };
